c76592f8a4c6932498b86a94e7224bba5b611639
Coral Solomon <coralsolomon179@gmail.com>
2020-10-19 20:14:50 +0300

    Updates

---
+++ precache-manifest.c2e70b8d48bbcc784e2d4954d665ebf6.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0dac4e0bcdfe6760d88a1f591b98ee9",
+    "url": "/Movies/index.html"
+  },
+  {
+    "revision": "5f93b5542cff9c26a7bd",
+    "url": "/Movies/static/css/2.83bfff73.chunk.css"
+  },
+  {
+    "revision": "b7923f78b9bc3235d84a",
+    "url": "/Movies/static/css/main.3a8ef5ba.chunk.css"
+  },
+  {
+    "revision": "5f93b5542cff9c26a7bd",
+    "url": "/Movies/static/js/2.57c0d251.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Movies/static/js/2.57c0d251.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7923f78b9bc3235d84a",
+    "url": "/Movies/static/js/main.cc1540cf.chunk.js"
+  },
+  {
+    "revision": "677b2be50562ccb6f395",
+    "url": "/Movies/static/js/runtime-main.0174cf4d.js"
+  },
+  {
+    "revision": "4b986d16c11a6e7b7fdf3a667d0027fd",
+    "url": "/Movies/static/media/02.4b986d16.png"
+  },
+  {
+    "revision": "13ed8ae2713990bdbb6144bf193ef9b2",
+    "url": "/Movies/static/media/Thor-2.13ed8ae2.jpg"
+  },
+  {
+    "revision": "9da23ee69736a0e69ef71dad40eaf876",
+    "url": "/Movies/static/media/about.9da23ee6.png"
+  },
+  {
+    "revision": "63816f99d68980cd403b056294fd497e",
+    "url": "/Movies/static/media/america.63816f99.jpg"
+  },
+  {
+    "revision": "de91132c1ba9b6e1c21afade9a13fa7e",
+    "url": "/Movies/static/media/home.de91132c.png"
+  },
+  {
+    "revision": "2581596e3b69f817c95e1d632b506683",
+    "url": "/Movies/static/media/left-arrow.2581596e.png"
+  },
+  {
+    "revision": "fcee831d37a3dd3b2093c8fc147e2ff1",
+    "url": "/Movies/static/media/movie.fcee831d.png"
+  },
+  {
+    "revision": "469ac9962ea8f6f1dc44607dd2554fdd",
+    "url": "/Movies/static/media/right-arrow.469ac996.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Movies/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Movies/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Movies/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Movies/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "07a0d2a735ad0f42b04ce7d65afe6a4d",
+    "url": "/Movies/static/media/spider.07a0d2a7.png"
+  },
+  {
+    "revision": "cd106983c6ae9b8273c5f811728bb52c",
+    "url": "/Movies/static/media/star.cd106983.jpeg"
+  }
+]);
